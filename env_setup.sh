fae31ed1c81afb11c9f6cb3b7a593f66409e59d5
Sebastian <dev@findusl.de>
2025-08-23 10:53:44 +0200

    Add setup script for codex

---
+++ env_setup.sh
@@ -0,0 +1,13 @@
+WORKSPACE_DIR="$(pwd)"
+export ANDROID_SDK_ROOT=$HOME/android-sdk
+mkdir -p "$ANDROID_SDK_ROOT/cmdline-tools"
+cd "$ANDROID_SDK_ROOT/cmdline-tools"
+wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+unzip commandlinetools-linux-*_latest.zip
+rm commandlinetools-linux-*_latest.zip
+mv cmdline-tools latest
+export PATH="$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$ANDROID_SDK_ROOT/platform-tools:$PATH"
+/bin/bash -c "yes | sdkmanager --licenses"
+sdkmanager "platform-tools" "platforms;android-35" "build-tools;35.0.0"
+cd $WORKSPACE_DIR
+./gradlew :composeApp:compileDebugSources
