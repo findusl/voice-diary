8a829592870f905b920c39af3def5a5ce4b3c937
Sebastian <dev@findusl.de>
2025-08-30 12:09:45 +0200

    Annotate core interfaces as Stable

--- composeApp/src/commonMain/kotlin/de/lehrbaum/voiry/audio/Transcriber.kt
+++ composeApp/src/commonMain/kotlin/de/lehrbaum/voiry/audio/Transcriber.kt
@@ -1,8 +1,10 @@
 package de.lehrbaum.voiry.audio
 
+import androidx.compose.runtime.Stable
 import kotlinx.io.Buffer
 
 /** Abstraction for turning audio into text. */
+@Stable
 interface Transcriber {
 	/**
 	 * Transcribes the given [buffer] and returns plain text.
