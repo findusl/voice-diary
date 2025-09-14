8c416072eb7b06d7246bb8a5dc991123c3d8b6ba
Sebastian <dev@findusl.de>
2025-09-14 12:41:12 +0200

    Initialize app dir on android

--- composeApp/src/androidMain/kotlin/de/lehrbaum/voiry/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/de/lehrbaum/voiry/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import ca.gosyer.appdirs.impl.attachAppDirs
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		enableEdgeToEdge()
 		super.onCreate(savedInstanceState)
+
+		application.attachAppDirs()
 
 		initLogging()
 
